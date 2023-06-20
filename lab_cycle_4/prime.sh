bbb25718f98e579843e408d1d9370cfda5f9878a
TinuBose <117371766+TinuBose@users.noreply.github.com>
2023-06-20 20:07:36 +0530

    Update prime.sh

--- lab_cycle_4/prime.sh
+++ lab_cycle_4/prime.sh
@@ -1,34 +1,22 @@
 #!/bin/bash
 
-# Get the range from the user
-echo "Enter the starting number of the range:"
-read start
-echo "Enter the ending number of the range:"
-read end
+# Get the lower and upper limits of the range from the user
+read -p "Enter the lower limit: " lower
+read -p "Enter the upper limit: " upper
 
-# Initialize the count to 0
+# Initialize the count of prime numbers
 count=0
 
-# Loop through each number in the range
-for (( num=start; num<=end; num++ ))
-do
-    # Check if the current number is prime
-    is_prime=true
-    for (( i=2; i<num; i++ ))
-    do
-        if [ $((num%i)) -eq 0 ]
-        then
-            is_prime=false
-            break
-        fi
-    done
+# Loop through all the numbers in the range
+for num in $(seq $lower $upper); do
 
-    # If the current number is prime, increment the count
-    if [ $is_prime = true ]
-    then
-        ((count++))
-    fi
+  # Check if the number is prime
+  if [ $(factor $num | grep $num | cut -d ":" -f 2 | cut -d " " -f 2) -eq $num ]; then
+
+    # Increment the count of prime numbers
+    count=$((count + 1))
+  fi
 done
 
-# Display the count of prime numbers
-echo "There are $count prime numbers in the range $start to $end."
+# Print the number of prime numbers
+echo "There are $count prime numbers in the range $lower-$upper."
