297cdd705642f95af37a028e7da420d1cb587120
TinuBose <117371766+TinuBose@users.noreply.github.com>
2023-09-06 15:40:57 +0530

    Update prime.sh

--- lab_cycle_3/prime.sh
+++ lab_cycle_3/prime.sh
@@ -1,24 +1,26 @@
-#!/bin/bash
-
-read -p "Enter the range (start end): " start end
+read -p "Start and end : " start end
+if (( start == 1 ))
+then
+	start=$((start+1))
+fi
 count=0
+echo "prime numbers bw $start and $end = "
+for((i=start;i<=end;i++))
 
-for ((i=start; i<=end; i++)); do
-    is_prime=1
-
-    for ((j=2; j<=i/2; j++)); do
-        if (($i % $j == 0)); then
-            is_prime=0
-            break
-        fi
-    done
-
-    if (($is_prime == 1)); then
-    	count=$((count+1))
-        echo "$i"
-        
-        
-    fi
-    
+do
+	is_prime=1
+	for((j=2;j<=i/2;j++))
+	do
+		if(( $i % $j == 0 ))
+		then
+			is_prime=0
+			break
+		fi
+	done
+	if ((is_prime == 1))
+	then
+		echo  "${i}"
+		count=$((count+1))
+	fi
 done
-echo "total prime numbers : $count"
+echo "count = $count"
